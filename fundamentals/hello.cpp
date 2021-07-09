8f4e22fe391ef20fb81919f42e827ca7d9b8dde3
Lucas Silveira <lugano1995@gmail.com>
2021-07-09 01:59:31 -0300

    change comment's lang of hello2 file

--- fundamentals/hello.cpp
+++ fundamentals/hello.cpp
@@ -5,8 +5,8 @@ int main() {
   // scope resolution operator
   std::cout << "Hello C++";
   /*
-    std é um namespace. Para acessar algo dentro
-    do namespace, utilizamos o operador "::".
+    std is a namespace. For access some thing inside the namespace
+    We need use the "::" operator.
   */
   
   return 0;
