ba99856cc5409f234fad0955736ec56e14599868
Lucas Silveira <lugano1995@gmail.com>
2021-07-09 01:57:14 -0300

    create a hello2

---
+++ fundamentals/hello2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+  std::cout << "Hello C++" << std::endl;
+  printf("Hello C++\n"); // provided by cstdio lib (c)
+  puts("Hello C++");
+  
+  return 0;
+}
