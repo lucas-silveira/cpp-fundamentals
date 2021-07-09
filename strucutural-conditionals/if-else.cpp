72b8ad813f681dd2a295b4348c4a8b29bb7c87d4
Lucas Silveira <lugano1995@gmail.com>
2021-07-09 18:09:17 -0300

    strucutral conditionals: if-else

---
+++ strucutural-conditionals/if-else.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+  double finalGrade;
+  cout << "Please, enter the final grade: ";
+  cin >> finalGrade;
+
+  if (finalGrade >= 7)
+  {
+    cout << "Excellente grade" << endl;
+    cout << "Congrats!" << endl;
+  }
+  else // we can omit the {} block
+    cout << "Bad grade" << endl;
+
+  return 0;
+}
