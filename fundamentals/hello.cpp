80565fd0351e2bc5651a93a697ece88e770b8f59
Lucas Silveira <lugano1995@gmail.com>
2021-07-09 01:57:06 -0300

    add a info comment to hello file

--- fundamentals/hello.cpp
+++ fundamentals/hello.cpp
@@ -2,7 +2,12 @@
 #include <iostream>
 
 int main() {
+  // scope resolution operator
   std::cout << "Hello C++";
+  /*
+    std é um namespace. Para acessar algo dentro
+    do namespace, utilizamos o operador "::".
+  */
   
   return 0;
 }
