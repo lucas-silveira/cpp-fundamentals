bb9cfb9039f25cfab3c4d5b6bb8814ecf725f818
Lucas Silveira <lugano1995@gmail.com>
2021-07-19 23:37:43 -0300

    memory pointer reference

---
+++ memory-pointer-reference/reference-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 10;
+
+  cout << a << endl; // value
+  cout << &a << endl; // pointer
+
+  string name = "Paul";
+  cout << name << "\t" << &name << endl;
+
+  return 0;
+}
