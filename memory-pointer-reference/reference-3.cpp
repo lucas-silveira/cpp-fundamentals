302d37bffa5ac5f4be24bc7cecaad3559338dfe8
Lucas Silveira <lugano1995@gmail.com>
2021-07-19 23:56:31 -0300

    memory pointer reference #3

---
+++ memory-pointer-reference/reference-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Once We have set a memory reference, It's not possible to change the reference
+  string greeting = "Hi!";
+  string &ref = greeting; // set memory reference
+
+  cout << greeting << "\t" << ref << endl;
+
+  string name = "Peter";
+  ref = name; // set value
+
+  cout << name << "\t" << ref << "\t" << greeting << endl;
+  cout << &name << "\t" << &ref << "\t" << &greeting << endl;
+
+  return 0;
+}
