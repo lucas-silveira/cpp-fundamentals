79646903b27278cbdcc9669012d3a50e1b6ef3db
Lucas Silveira <lugano1995@gmail.com>
2022-03-14 22:20:34 -0300

    static var

---
+++ functions/static-var.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int nextId() {
+  /*
+    Ensure that the variable will only be initialized once.
+    That is, the state will be shared between function calls.
+  */
+  static int id = 1;
+
+  return id++;
+}
+
+int main()
+{
+  cout << nextId() << endl; // 1
+  cout << nextId() << endl; // 2
+  cout << nextId() << endl; // 3
+  cout << nextId() << endl; // 4
+
+  return 0;
+}
