8b6e8538108d175f94c48c7496c8b96ad28afe3c
Lucas Silveira <lugano1995@gmail.com>
2022-03-14 22:27:18 -0300

    main args

---
+++ functions/main-args.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  // argc indicates the arguments amount passed as paramater
+  cout << argc << endl;
+
+  // argv is an array of arguments
+  cout << argv[0] << endl;
+  
+  return 0;
+}
