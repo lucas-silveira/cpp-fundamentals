6b48a2c9c03d55cc54003063ed193df051bf5990
Lucas Silveira <lugano1995@gmail.com>
2022-03-21 23:39:21 -0300

    arrays: matrix

---
+++ arrays/matrix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int matrix[3][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+  };
+
+  cout << matrix[1][2] << endl; // 6
+
+  return 0;
+}
