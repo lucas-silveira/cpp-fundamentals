6a38ff14bcca2287a75f6b817c43e91d9adaaa8c
Lucas Silveira <lugano1995@gmail.com>
2022-09-22 12:14:23 -0300

    challenges(medium): group equal chars

---
+++ algorithms/challenges/medium/group-equal-chars.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+/*
+  This solution has a time complexity O(nlgn) and a space complexity O(n)
+*/
+
+vector<vector<string>> groupStrings(vector<string> arr) {
+  map<string, vector<string>> groupedTable;
+  vector<vector<string>> groupedList;
+
+  for (int i = 0; i < arr.size(); i++) {
+    string copy = arr[i];
+    sort(copy.begin(), copy.end());
+
+    groupedTable[copy].push_back(arr[i]);
+  }
+
+  for (pair<string, vector<string>> entry : groupedTable) {
+    groupedList.push_back(entry.second);
+  }
+
+  return groupedList;
+}
+
+int main()
+{
+  vector<string> strings = {"142", "421", "9ab", "241", "ba9", "4211"};
+  vector<vector<string>> groupedStrings = groupStrings(strings);
+
+  for (vector<string> list : groupedStrings) {
+    for (string str : list) {
+      cout << str << ", ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
